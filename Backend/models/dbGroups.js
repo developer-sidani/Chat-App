9b3b35a3a60a7296449f39761fd37ceae5c9aeaa
developer-sidani <asidani88@gmail.com>
2021-04-16 04:08:38 +0300

    Revert "updates added backend"
    This reverts commit 5fc6d3b1bd73917737d5d47cb5c0be8b354410e4.

--- Backend/models/dbGroups.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const GroupSchema = mongoose.Schema({
-  groupName: String,
-  imageUrl: String,
-  admin: Array,
-  members: Array,
-});
-export default mongoose.model("GroupDetails", GroupSchema);
